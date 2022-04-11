b484150b539611a572c9e9993baa36388de94f9a
Bluette1 <marylene.sawyer@gmail.com>
2022-04-11 04:37:20 +0200

    Fix lint

--- src/index.js
+++ src/index.js
@@ -9,8 +9,6 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <Provider store={configureStore()}>
-    <>
-      <App />
-    </>
+    <App />
   </Provider>,
 );
